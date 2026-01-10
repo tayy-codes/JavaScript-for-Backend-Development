38b4f8a83eea61019f22bc001001a6b2accb1532
tayy-codes <tayyebaa123@gmail.com>
2026-01-10 16:37:08 +0000

    datatypes discussed

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,7 +4,30 @@
 //does not let user communicate with rest of webpage until popup is dismissed. 
 
 console.log(3
-    + 3
-)                                   //code reusability should be high
+    + 3)                                   //code reusability should be high
 
 console.log("Tayyeba")
+
+let name = "Tayyeba"      // string
+let age = 20              // number
+let isLoggedIn = true     // boolean value
+//let state;                // undefined value; declared but not defined
+let state = null          // null defines an empty value 
+//let state = ""            // this isn't empty value rather it's datatype is defined as string
+
+// number => 2 to the power 53
+//bigint
+//string = ""
+//boolean => true/false
+//null
+//undefined
+//symbol => immutable and unique; returns a unique symbol value each time it is called.
+
+//object  
+
+console.log(typeof age);     //number; typeof keyword describes the datatype of variable
+console.log(typeof null);    //object;      null is an object
+console.log(typeof undefined);     //undefined;      undefined is one of datatypes
+
+
+//PREFER ECMASCRIPT FOR DOCUMENTATION
