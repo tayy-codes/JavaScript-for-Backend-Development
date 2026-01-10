d19d4ed6374143ae605bd8b6f4cf903d916e054d
tayy-codes <tayyebaa123@gmail.com>
2026-01-10 16:07:15 +0000

    datatypes file created

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"   // treats all the JS code as standards according to new version
+
+//alert("Ok")  //to display a pop-up dialog box with a specified message; no return value;
+//does not let user communicate with rest of webpage until popup is dismissed. 
+
+console.log(3
+    + 3
+)                                   //code reusability should be high
+
+console.log("Tayyeba")
