54aa0ad3c7214347f4c119f7486669d3c3dffc3f
tayy-codes <tayyebaa123@gmail.com>
2026-01-11 17:34:35 +0000

    datatype conversions done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,75 @@
+//let score = "33"
+
+/*console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));   //number; 
+                                      //when score=33
+*/
+
+/*
+let score = "33abc"    
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));  //number; even when score=33abc
+console.log(valueInNumber);          // NaN; "Not a Number": special datatype
+*/
+
+/*
+let score = null 
+console.log(typeof score);            //object
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));   //number; when score=null
+console.log(valueInNumber);           //0 
+*/
+
+/*
+let score = undefined 
+console.log(typeof score);            //undefined
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));   //number; when score=undefined
+console.log(valueInNumber);           //NaN; "Not a Number"
+*/
+
+/*
+let score = true 
+console.log(typeof score);            //boolean
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));   //number; when score=true
+console.log(valueInNumber);           //1;   '0' when score='false'
+*/ 
+
+/*
+let score = "Tayyeba" 
+console.log(typeof score);            //string
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));   //number; when score="Tayyeba"
+console.log(valueInNumber);           //NaN; "Not a Number"
+*/
+
+/*
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);      // true; when isLoggedIn=1
+*/
+
+/*
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);      // false; when isLoggedIn=""
+*/
+
+/*
+let isLoggedIn = "Tayyeba"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);      // true; when isLoggedIn isn't
+                                   //empty and contains value
+*/
+
+let num2 = 33
+let stringNum2 = String(num2)
+console.log(stringNum2);         //33
+console.log(typeof stringNum2);  //string
+
+
+
